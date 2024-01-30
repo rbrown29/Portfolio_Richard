45b4f38b3328b9950a5591c8b27484cb03312063
rbrown29 <53094729+rbrown29@users.noreply.github.com>
2024-01-29 21:09:02 -0800

    update travel pages

---
+++ travel.js
@@ -0,0 +1,128 @@
+$(document).ready(function() {
+  $('#gadget').on('click', function(e) {
+   window.open('https://splendorous-pika-c3c713.netlify.app/', '_blank');
+  }
+  
+)});
+
+$(document).ready(function() {
+  $('#gadget1').on('click', function(e) {
+    window.open('https://github.com/rbrown29/Gadget-Junkie', '_blank');
+  }
+)});
+
+$(document).ready(function() {
+  $('#job').on('click', function(e) {
+    window.open('https://job-application-tracker.onrender.com/landing', '_blank');
+  }
+)});
+
+$(document).ready(function() {
+  $('#job1').on('click', function(e) {
+    window.open('https://github.com/rbrown29/Job-Application-Tracker', '_blank');
+  }
+)});
+
+$(document).ready(function() {
+  $('#nasa').on('click', function(e) {
+    window.open('https://nasa-mission-control.onrender.com/', '_blank');
+  }
+)});
+
+$(document).ready(function() {
+  $('#nasa1').on('click', function(e) {
+    window.open('https://github.com/rbrown29/Nasa-Mission-Control', '_blank');
+  }
+)});
+
+$(document).ready(function() {
+  $('#oregon').on('click', function(e) {
+    window.open('https://hike-neqm.onrender.com/', '_blank');
+  }
+)});
+
+$(document).ready(function() {
+  $('#oregon1').on('click', function(e) {
+    window.open('https://github.com/rbrown29/sturdy-octo-computing-machine', '_blank');
+  }
+)});
+
+$(document).ready(function() {
+  $('#node').on('click', function(e) {
+    window.open('https://fitness-den6.onrender.com/', '_blank');
+  }
+)});
+
+$(document).ready(function() {
+  $('#node1').on('click', function(e) {
+    window.open('https://github.com/rbrown29/node-fitness', '_blank');
+  }
+)});
+$(document).ready(function() {
+  $('#travel').on('click', function(e) {
+    window.open('https://locations-29of.onrender.com/', '_blank');
+  }
+)});
+
+$(document).ready(function() {
+  $('#travel1').on('click', function(e) {
+    window.open('https://github.com/rbrown29/Locations', '_blank');
+  }
+)});
+$(document).ready(function() {
+  $('#blog').on('click', function(e) {
+    window.open('https://blog-05el.onrender.com/', '_blank');
+  }
+)});
+
+$(document).ready(function() {
+  $('#blog1').on('click', function(e) {
+    window.open('https://github.com/rbrown29/blog', '_blank');
+  }
+)});
+$(document).ready(function() {
+  $('#music').on('click', function(e) {
+    window.open('https://musicapp-3xgy.onrender.com/', '_blank');
+  }
+)});
+
+$(document).ready(function() {
+  $('#music1').on('click', function(e) {
+    window.open('https://github.com/rbrown29/Music-App-Frontend', '_blank');
+  }
+)});
+$(document).ready(function() {
+  $('#chat').on('click', function(e) {
+    window.open('https://sweet-dodol-a631d4.netlify.app/', '_blank');
+  }
+)});
+
+$(document).ready(function() {
+  $('#chat1').on('click', function(e) {
+    window.open('https://github.com/rbrown29/terminal-chat', '_blank');
+  }
+)});
+
+$(document).ready(function() {
+  $('#bitcoin').on('click', function(e) {
+    window.open('https://bitcoinandcrypto-frontend.onrender.com/', '_blank');
+  }
+)});
+
+$(document).ready(function() {
+  $('#bitcoin1').on('click', function(e) {
+    window.open('https://github.com/rbrown29/Bitcoin-and-Crypto-Frontend', '_blank');
+  }
+)});
+
+$(document).ready(function() {
+  $('#planet').on('click', function(e) {
+    window.open('https://fascinating-jalebi-d96595.netlify.app/', '_blank');
+  }
+)});
+
+$(document).ready(function() {
+  $('#planet1').on('click', function(e) {
+    window.open('https://cyberpunk-portfolio.netlify.app/', '_blank');
+  }
+)});
